5834ac172c11b631f4b69f67f91fae9b7506ae38
Aandreba <aandrebafreelancer@gmail.com>
2023-05-22 18:38:02 +0200

    d2-p1

--- src/day2.zig
+++ src/day2.zig
@@ -1,15 +1,62 @@
+const std = @import("std");
+
+const File = std.fs.File;
+
 const Play = enum {
     Rock,
     Paper,
     Scissors,
 
+    fn score(self: Play) u8 {
+        return switch (self) {
+            .Rock => 1,
+            .Paper => 2,
+            .Scissors => 3,
+        };
+    }
+
+    fn from_name(char: u8) ?Play {
+        return switch (char) {
+            'X', 'A' => .Rock,
+            'Y', 'B' => .Paper,
+            'Z', 'C' => .Scissors,
+            else => null,
+        };
+    }
+
     fn battle(self: Play, other: Play) Result {
-        if (self == other) return;
-        
+        if (self == other) return .Draw;
+
         switch (self) {
             .Rock => {
-                if (other == .Paper)
-            }
+                if (other == .Paper) {
+                    return .Loss;
+                } else if (other == .Scissors) {
+                    return .Win;
+                } else {
+                    unreachable;
+                }
+            },
+
+            .Paper => {
+                if (other == .Rock) {
+                    return .Win;
+                } else if (other == .Scissors) {
+                    return .Loss;
+                } else {
+                    unreachable;
+                }
+            },
+
+            .Scissors => {
+                if (other == .Rock) {
+                    return .Loss;
+                } else if (other == .Paper) {
+                    return .Win;
+                } else {
+                    unreachable;
+                }
+            },
         }
     }
 };
@@ -18,6 +65,14 @@ const Result = enum {
     Win,
     Loss,
     Draw,
+
+    fn score(self: Result) u8 {
+        return switch (self) {
+            .Win => 6,
+            .Draw => 3,
+            .Loss => 0,
+        };
+    }
 
     fn reverse(self: Result) Result {
         return switch (self) {
@@ -33,7 +88,32 @@ pub fn day2() anyerror!void {
 }
 
 pub fn part_one() anyerror!void {
-    const lhs = Play.Paper;
-    const rhs = Play.Scissors;
-    _ = lhs.battle(rhs);
+    const cwd = std.fs.cwd();
+
+    // Open file handle
+    const file = try cwd.openFile("exercices/two.txt", .{});
+    defer file.close();
+
+    var buffered = std.io.bufferedReader(file.reader());
+    const reader = buffered.reader();
+
+    var score: u64 = 0;
+    while (true) {
+        const opponent = Play.from_name(reader.readByte() catch |err| {
+            if (err == error.EndOfStream) {
+                break;
+            } else {
+                return err;
+            }
+        }) orelse unreachable;
+        try reader.skipBytes(1, .{ .buf_size = 1 });
+        const player = Play.from_name(try reader.readByte()) orelse unreachable;
+        try reader.skipBytes(1, .{ .buf_size = 1 });
+
+        const play_points = player.score();
+        const battle_pts = player.battle(opponent).score();
+        score += play_points + battle_pts;
+    }
+
+    std.debug.print("{}\n", .{score});
 }
